827991f39f6108efeee333535b8dd84a38ea4951
Sanshiroh SAKAI <sansyo@gmail.com>
2024-09-11 07:26:31 +0900

    Update: 一覧をもとに詳細情報をまとめて取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import Layout from '@/components/layout/Layout'
-import { getAllPokemon } from '@/utils/pokemon'
+import type { Pokemon } from '@/types/pokemon'
+import { getAllPokemon, getPokemon } from '@/utils/pokemon'
 
 export default function Home() {
+  const router = useRouter()
   const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
   const [offset, setOffset] = useState(0)
+  const [page, setPage] = useState(1)
 
   /**
    * 前のページへ移動
@@ -13,6 +17,8 @@ export default function Home() {
   const handlePrevButton = () => {
     if (offset === 0) return
     setOffset((prev) => prev - 20)
+    setPage((prev) => prev - 1)
+    router.push(`/?page=${page - 1}`)
   }
 
   /**
@@ -20,6 +26,8 @@ export default function Home() {
    */
   const handleNextButton = () => {
     setOffset((prev) => prev + 20)
+    setPage((prev) => prev + 1)
+    router.push(`/?page=${page + 1}`)
   }
 
   // ポケモン一覧を取得する useQuery
@@ -28,20 +36,66 @@ export default function Home() {
     queryFn: () => getAllPokemon(initialUrl, offset),
   })
 
+  // ポケモンの詳細情報を一覧からまとめて取得するラッパー関数
+  const loadPokemon = async (data: Pokemon[]) => {
+    const _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        const pokemonRecord = getPokemon(pokemon.url)
+        return pokemonRecord
+      }),
+    )
+    return _pokemonData
+  }
+
+  // ポケモンの詳細情報を取得する useQuery
+  const { data: pokemonData, isLoading: isPokemonDataLoading } = useQuery({
+    queryKey: ['pokemonData', pokemonList, pokemonList?.results],
+    queryFn: () => loadPokemon(pokemonList?.results ?? []),
+    enabled: !!pokemonList,
+  })
+
   return (
     <Layout>
       <main className="p-8">
         <h1 className="mb-4 text-2xl font-bold">ポケモン一覧</h1>
-        {isPokemonListLoading ? (
+        {isPokemonListLoading || isPokemonDataLoading ? (
           <p>Loading...</p>
         ) : (
-          <ul>
-            {pokemonList?.results.map((pokemon) => (
-              <li key={pokemon.name} className="mb-2 rounded border p-3">
-                {pokemon.name}
-              </li>
-            ))}
-          </ul>
+          <>
+            <div className="mb-2 flex gap-2">
+              <button
+                className="rounded bg-blue-500 px-4 py-2 text-white"
+                onClick={handlePrevButton}
+              >
+                前へ
+              </button>
+              <button
+                className="rounded bg-blue-500 px-4 py-2 text-white"
+                onClick={handleNextButton}
+              >
+                次へ
+              </button>
+            </div>
+            <ul>
+              {/* {pokemonList?.results.map((pokemon) => (
+                <li key={pokemon.name} className="mb-2 rounded border p-3">
+                  {pokemon.name}
+                </li>
+              ))} */}
+              {pokemonData?.map((pokemon) => (
+                <li key={pokemon.id}>
+                  <p>{pokemon.name}</p>
+                  <img
+                    src={
+                      pokemon.sprites.other['official-artwork'].front_default
+                    }
+                    alt=""
+                    className="size-[90px]"
+                  />
+                </li>
+              ))}
+            </ul>
+          </>
         )}
       </main>
     </Layout>
